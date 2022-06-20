b0eff6d0422e64736089e83b759ccf8778c996a3
Pierre <prg@protonmail.ch>
2022-06-20 13:51:49 +0200

    cannot do macos already host

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import blue_print_pos
 import blue_thermal_printer
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  BluePrintPosPlugin.register(with: registry.registrar(forPlugin: "BluePrintPosPlugin"))
   BlueThermalPrinterPlugin.register(with: registry.registrar(forPlugin: "BlueThermalPrinterPlugin"))
 }
